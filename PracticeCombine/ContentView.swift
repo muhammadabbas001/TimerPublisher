7c6be328e82be38966485fdf2538c78c07945f42
Abbas <muhammdabbas.cs@gmail.com>
2020-11-16 17:41:50 +0500

    Adding the Data Task Publisher

--- PracticeCombine/ContentView.swift
+++ PracticeCombine/ContentView.swift
@@ -7,12 +7,20 @@
 
 import SwiftUI
 
+//Model
+struct User: Decodable, Identifiable{
+    let id: Int
+    let name: String
+}
+
 //ViewModels
 import Combine
 
 final class ViewModel: ObservableObject{
     @Published var time = ""
-    private var anyCancelable: AnyCancellable?
+    @Published var users = [User]()
+    
+    private var cancelable = Set<AnyCancellable>()
     
     var formatter: DateFormatter = {
         let df = DateFormatter()
@@ -22,16 +30,37 @@ final class ViewModel: ObservableObject{
     
     init(){
         setupPublisher()
+        setupDataTaskPublisher()
+    }
+    
+    private func setupDataTaskPublisher(){
+        let url = URL(string: "https://jsonplaceholder.typicode.com/users")!
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap{(data, response) in
+                guard let httpResponse = response as? HTTPURLResponse,
+                      httpResponse.statusCode == 200 else{
+                    throw URLError(.badServerResponse)
+                }
+                return data
+            }
+            .decode(type: [User].self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { _ in }){users in
+                self.users.append(contentsOf: users)
+            }
+            .store(in: &cancelable)
     }
     
     //Publisher
     private func setupPublisher(){
-        anyCancelable = Timer.publish(every: 1, on: .main, in: .default)
+        Timer.publish(every: 1, on: .main, in: .default)
             .autoconnect()
             .receive(on: RunLoop.main)
             .sink{value in
                 self.time = self.formatter.string(from: value)
             }
+            .store(in: &cancelable)
     }
 }
 
@@ -41,5 +70,8 @@ struct ContentView: View {
     var body: some View {
         Text(viewModel.time)
             .padding()
+        List(viewModel.users){user in
+            Text("\(user.name)")
+        }
     }
 }
