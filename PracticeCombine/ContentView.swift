01ef59cc3d986cf4ff7728fe3be067f95b854268
Abbas <muhammdabbas.cs@gmail.com>
2020-11-16 14:13:50 +0500

    Initial Commit

---
+++ PracticeCombine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PracticeCombine
+//
+//  Created by Muhammad Abbas on 11/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
