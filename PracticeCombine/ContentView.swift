218448d065df50f4689a1d5e864426525baaad38
Abbas <muhammdabbas.cs@gmail.com>
2020-11-16 14:46:48 +0500

    first commit

--- PracticeCombine/ContentView.swift
+++ PracticeCombine/ContentView.swift
@@ -6,16 +6,36 @@
 //
 
 import SwiftUI
+import Combine
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+final class ViewModel: ObservableObject{
+    @Published var time = ""
+    private var anyCancelable: AnyCancellable?
+    
+    var formatter: DateFormatter = {
+        let df = DateFormatter()
+        df.timeStyle = .medium
+        return df
+    }()
+    
+    init(){
+        setupPublisher()
+    }
+    
+    private func setupPublisher(){
+        anyCancelable = Timer.publish(every: 1, on: .main, in: .default)
+            .autoconnect()
+            .receive(on: RunLoop.main)
+            .sink{value in
+                self.time = self.formatter.string(from: value)
+            }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+    var body: some View {
+        Text(viewModel.time)
+            .padding()
     }
 }
